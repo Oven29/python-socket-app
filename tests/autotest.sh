579faf18f0c9ee747460108eeeb25e5c70669d2c
Oven29 <ovchinno.ivan@gmail.com>
2025-05-31 20:54:27 +0300

    [tcp|feat] - Create autotest.sh

---
+++ tests/autotest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE_ORIGINAL="sent"
+FILE_RECEIVED="received"
+FILE_SIZE_MB=2
+SERVER_PORT=$((RANDOM + 10000))
+
+dd if=/dev/urandom of="$FILE_ORIGINAL" bs=1M count=$FILE_SIZE_MB status=none
+
+python ./src/server.py "$SERVER_PORT" "$FILE_ORIGINAL" &
+SERVER_PID=$!
+
+sleep 1
+
+python ./src/client.py "$SERVER_PORT" "$FILE_RECEIVED"
+
+wait $SERVER_PID
+
+if cmp -s "$FILE_ORIGINAL" "$FILE_RECEIVED"; then
+    echo "Test passed"
+    EXIT_CODE=0
+else
+    echo "Wrong test"
+    EXIT_CODE=1
+fi
+
+rm -f "$FILE_ORIGINAL" "$FILE_RECEIVED"
+
+exit $EXIT_CODE
