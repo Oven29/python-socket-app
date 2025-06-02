e183476fbeb64c624a68df30646eec2fc4d97df4
Oven29 <ovchinno.ivan@gmail.com>
2025-06-03 00:06:26 +0300

    [udp|feat] - Create udp server/client

--- src/client.py
+++ src/client.py
@@ -1,9 +1,27 @@
 import logging
 import socket
+import struct
 import sys
+from typing import List
 
 
 logger = logging.getLogger(__name__)
+
+
+def write_file(filename: str, data: List[bytes]) -> None:
+    """
+        Write the data to the file.
+
+        Args:
+            filename (str): The name of the file to write to.
+            data (List of bytes): The pickle chunks to write to file.
+    """
+    chunks = [struct.unpack('!I1024s', chunk) for chunk in data]
+    chunks.sort(key=lambda x: x[0])
+
+    with open(filename, 'wb') as f:
+        for _, chunk in chunks:
+            f.write(chunk)
 
 
 def run_client(host: str, port: int, filename: str) -> None:
@@ -15,16 +33,28 @@ def run_client(host: str, port: int, filename: str) -> None:
             port (int): The port to connect to.
             filename (str): The name of the file to receive.
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        addr = s.getpeername()
-        logger.info(f'requesting from {addr}')
-        logger.info('downloading...')
-        with open(filename, "wb") as f:
-            data = s.recv(1024)
-            while data:
-                f.write(data)
-                data = s.recv(1024)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        server_addr = (host, port)
+        logger.info(f'requesting from {server_addr}')
+
+        s.settimeout(5)
+        i = 0
+        packets = []
+        data = None
+
+        received_size = struct.calcsize('!I1024s')
+
+        while True:
+            s.sendto(f'RECEIVE {i}'.encode(), server_addr)
+            data, _ = s.recvfrom(received_size)
+            if not data:
+                continue
+            if data == b'__END__':
+                break
+            packets.append(data)
+            i += 1
+
+        write_file(filename, packets)
         logger.info(f'downloaded as "{filename}"')
 
 
@@ -35,7 +65,7 @@ def main() -> None:
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    assert len(sys.argv) == 3, "Usage: python client.py <port> <filename>"
+    assert len(sys.argv) == 3, 'Usage: python client.py <port> <filename>'
     port = int(sys.argv[1])
     filaneme = sys.argv[2]
 
--- src/server.py
+++ src/server.py
@@ -1,9 +1,35 @@
 import logging
 import socket
+import struct
 import sys
+from typing import List
 
 
 logger = logging.getLogger(__name__)
+
+
+def split_file_into_packages(filename: str) -> List[bytes]:
+    """
+        Split the file into binary packets, including chunk`s index and chunk`s data.
+
+        Args:
+            filename (str): The name of the file to sent.
+
+        Returns:
+            List: A list of binary packets.
+    """
+    file_packets = []
+
+    with open(filename, "rb") as f:
+        chunk = f.read(1024)
+        i = 0
+        while chunk:
+            packet = struct.pack('!I1024s', i, chunk)
+            file_packets.append(packet)
+            i += 1
+            chunk = f.read(1024)
+
+    return file_packets
 
 
 def run_server(host: str, port: int, filename: str) -> None:
@@ -16,19 +42,33 @@ def run_server(host: str, port: int, filename: str) -> None:
             filename (str): The name of the file to sent.
     """
     logger.info(f'serving "{filename}" on {host}:{port}')
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            logger.info(f'request from {addr}')
-            logger.info('sending...')
-            with open(filename, "rb") as f:
-                data = f.read(1024)
-                while data:
-                    conn.sendall(data)
-                    data = f.read(1024)
-        logger.info(f'finished to send {addr}')
+        logger.info('waiting for request...')
+        
+        file_packets = split_file_into_packages(filename)
+
+        while True:
+            data, addr = s.recvfrom(1024)
+
+            # Проверка запроса
+            decoded_data = data.decode()            
+            logger.info(f'received from {addr} data: {decoded_data}')
+
+            if not " " in decoded_data:
+                continue
+            flag, index = decoded_data.split(" ")
+            if not index.isdigit():
+                continue
+
+            if flag == 'RECEIVE' and int(index) < len(file_packets):
+                logger.info(f'sending {index} packet to {addr}...')
+                s.sendto(file_packets[int(index)], addr)
+            elif flag == 'RECEIVE':
+                logger.info(f'finished to send on {addr}')
+                s.sendto(b'__END__', addr)
+                break
 
 
 def main() -> None:
@@ -38,7 +78,7 @@ def main() -> None:
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    assert len(sys.argv) == 3, "Usage: python server.py <port> <filename>"
+    assert len(sys.argv) == 3, 'Usage: python server.py <port> <filename>'
     port = int(sys.argv[1])
     filaneme = sys.argv[2]
 
